af7e3fd821135ffd40d11efa13bd7154a5715ef7
Whiting <whitinghtennis@gmail.com>
2021-09-07 22:17:52 -0700

    commit tree e28e3e918b2c0bacd8534ec16ebf825757b90c2e

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+    def create
+        # create a new user; save their hashed password in the database; save the user's ID in the session hash; and return the user object in the JSON response.
+    end
+
+    def show
+        # If the user is authenticated, return the user object in the JSON response.
+    end
+
+
+end
